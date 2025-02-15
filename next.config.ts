4972071b97909467e9c75e6e52dc2d2e2433dce9
Marcin Zasadzki <zasadzkimarcin@gmail.com>
2025-02-15 23:25:12 +0900

    chore: update nextjs config file

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/sequence-maker",
 };
 
 export default nextConfig;
