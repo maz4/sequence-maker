bb90f556cb1249eab960bf8519279a71e1113f51
Marcin Zasadzki <zasadzkimarcin@gmail.com>
2025-02-15 16:45:19 +0900

    feat: import components to the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,11 @@
-import AuBatido from "@/components/svgs/AuBatido";
+import Hero from "@/components/Hero";
+import MovesList from "@/components/MovesList";
 
 export default function Home() {
   return (
-    <div className="min-h-screen max-w-screen-xl mx-auto">
-      <main className="container mx-auto px-4 py-8">
-        <div className="flex flex-col md:flex-row md:items-center md:space-x-8">
-          <div className="flex justify-center w-full md:w-1/2 mb-8 md:mb-0">
-            <AuBatido />
-          </div>
-          <div className="w-full md:w-1/2">
-            <h1 className="text-3xl md:text-4xl font-bold mb-4">
-              Sequence Capoeira Movements
-            </h1>
-            <p className="text-lg mb-8">
-              Explore the dynamic world of Capoeira with our random movement
-              selector. Enhance your training and expand your repertoire with
-              just a click.
-            </p>
-          </div>
-        </div>
-      </main>
+    <div className="min-h-screen max-w-screen-xl mx-auto flex flex-col items-center">
+      <Hero />
+      <MovesList />
     </div>
   );
 }
