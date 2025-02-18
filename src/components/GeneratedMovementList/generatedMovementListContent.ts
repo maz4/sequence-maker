f59efbcb3e277d601bf5f1363c51584025555495
Marcin Zasadzki <zasadzkimarcin@gmail.com>
2025-02-18 22:05:20 +0900

    feat: add language selection to generat movement list

---
+++ src/components/GeneratedMovementList/generatedMovementListContent.ts
@@ -0,0 +1,20 @@
+export const content = {
+  en: {
+    sectionTitle: "Capoeira Moves",
+    subSectionH3: "Selected Moves:",
+    generateMovesButton: "Generate Moves",
+    clearButton: "Clear",
+  },
+  jp: {
+    sectionTitle: "カポエイラの動き",
+    subSectionH3: "選択された動き:",
+    generateMovesButton: "動きを生成",
+    clearButton: "クリア",
+  },
+  pt: {
+    sectionTitle: "Movimentos de Capoeira",
+    subSectionH3: "Movimentos Selecionados:",
+    generateMovesButton: "Gerar Movimentos",
+    clearButton: "Limpar",
+  },
+};
