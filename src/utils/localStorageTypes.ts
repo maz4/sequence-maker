8ddfe58dfaebd6b6b9b242efb47b0d5b43302a6b
Marcin Zasadzki <zasadzkimarcin@gmail.com>
2025-03-20 12:07:42 +0900

    feat: define and save new key in local storage

--- src/utils/localStorageTypes.ts
+++ src/utils/localStorageTypes.ts
@@ -1,6 +1,8 @@
+import { FeatureFlags } from "@/consts/featureFlags";
 import { Language } from "@/consts/languages";
 
 export interface StorageData {
   language: Language;
+  featureFlags: FeatureFlags;
   [key: string]: unknown;
 }
