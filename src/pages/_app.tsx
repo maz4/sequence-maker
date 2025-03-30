117e61a149654ed20c02ecd93145cb92447f34e1
Marcin Zasadzki <zasadzkimarcin@gmail.com>
2025-03-30 17:10:46 +0900

    feat: fix type issues

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import { LanguageProvider } from "@/contexts/LanguageContext";
 import { FeatureFlagsProvider } from "@/contexts/FeatureFlagsContext";
+import "@/app/globals.css";
 
 export default function PagesLayout({ Component, pageProps }: AppProps) {
   return (
